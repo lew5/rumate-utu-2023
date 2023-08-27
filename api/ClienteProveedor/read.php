13f35cab2956a952e4321b0d2c094a43fadec9ca
Leandro <lewaccont@gmail.com>
2023-08-27 17:22:39 -0300

    Update read.php

--- api/ClienteProveedor/read.php
+++ api/ClienteProveedor/read.php
@@ -2,9 +2,9 @@
 header("Content-Type: application/json");
 $clienteProveedor = App::resolve("ClienteProveedor");
 if (isset($_GET['id'])) {
-  $response = json_encode($clientepProveedor->getById($_GET['id']));
+  $response = json_encode($clienteProveedor->getById($_GET['id']));
 } else {
-  $response = json_encode($clientepProveedor->getAll());
+  $response = json_encode($clienteProveedor->getAll());
 }
 print $response;
 ?>
