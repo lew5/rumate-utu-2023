e43bf1ee784bb34b957de0f3229cdd308134651f
Leandro <53401001+lew5@users.noreply.github.com>
2023-10-18 10:13:08 -0300

    UTU 2 Update LotePostulaRemateRepository.php

--- App/Repositories/LotePostulaRemateRepository.php
+++ App/Repositories/LotePostulaRemateRepository.php
@@ -1,51 +1,39 @@
 <?php
 
-class LotePostulaRemateRepository extends BaseRepository implements IRepositoryInterface
+class LotePostulaRemateRepository extends Repository
 {
-  private $_db;
-  private $table = "lotes_postulan_remates";
-  private $obj = "LotePostulaRemate";
-
-  public function find($idRemate)
+  public function __construct()
   {
-    return $this->readOne($idRemate, $this->table, "id_remate_lote_postula_remate", $this->obj);
+    parent::__construct("lotes_postulan_remates","id_remate_lote_postula_remate","LotePostulaRemate");
   }
 
-  public function findAll()
+  public function find()
   {
-    return $this->readAll($this->table, $this->obj);
+    return $this->read();
   }
 
-  public function create($lotePostulaRemateModel)
+  public function findByRemateId($id)
   {
-    $this->_db = DataBase::get();
-    $stm = $this->_db->prepare(
-      "INSERT INTO LOTES_POSTULAN_REMATES (
-        id_remate_lote_postula_remate,
-        id_lote_lote_postula_remate
-      ) 
-      VALUES (
-        :id_remate,
-        :id_lote
-      )"
+    return $this->read(
+      [
+        "$this->idColumn" => $id
+      ]
     );
-    $stm->execute([
-      'id_remate' => $lotePostulaRemateModel->getIdRemate(),
-      'id_lote' => $lotePostulaRemateModel->getIdLote()
-    ]);
-    $this->_db = null;
   }
 
-  public function update($model)
+  public function addLoteToRemate($data)
   {
-    // Implementa la lógica de actualización si es necesario
+    $this->create($data);
   }
 
-  public function delete($idRemate)
+  public function updateLoteToRemate($id, $data)
   {
-    return $this->deleteOne($idRemate, $this->table, "id_remate_lote_postula_remate");
+    $this->update($id, $data);
+  }
+
+  public function deleteLoteToRemate($id)
+  {
+    $this->delete($id);
   }
 }
-
-
 ?>
