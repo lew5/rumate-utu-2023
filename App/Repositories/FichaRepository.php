15269d0938d22cea5dfba0215e3c4030c66fd3bb
Leandro <53401001+lew5@users.noreply.github.com>
2023-10-18 09:24:01 -0300

    UTU Update FichaRepository.php

--- App/Repositories/FichaRepository.php
+++ App/Repositories/FichaRepository.php
@@ -1,113 +1,40 @@
 <?php
 
-class FichaRepository implements IRepositoryInterface
+class FichaRepository extends Repository
 {
-  private $_db;
-
-  // OBTENER UNA FICHA
-  public function find($id)
+  public function __construct()
   {
-    $this->_db = DataBase::get();
-    $result = null;
-
-    $stm = $this->_db->prepare(
-      "SELECT * FROM fichas
-        WHERE id_ficha = :id"
-    );
-    $stm->execute([
-      'id' => $id
-    ]);
-
-    $data = $stm->fetchObject("Ficha");
-
-    if ($data) {
-      $result = $data;
-    }
-
-    $this->_db = null;
-
-    return $result;
+    parent::__construct("fichas");
   }
 
-  // OBTENER TODOS LOS FICHAS
-  public function findAll()
+  public function find()
   {
-    $this->_db = DataBase::get();
-    $result = [];
-
-    $stm = $this->_db->prepare("SELECT * FROM fichas");
-    $stm->execute();
-
-    $result = $stm->fetchAll(PDO::FETCH_CLASS, "Ficha");
-
-    $this->_db = null;
-
-    return $result;
+    return $this->read("Ficha");
   }
 
-
-  // CREAR UNA FICHA
-  public function create($fichaModel)
+  public function findById($id)
   {
-    $this->_db = DataBase::get();
-    $lastInsertId = null;
-
-    $stm = $this->_db->prepare(
-      "INSERT INTO fichas (
-          peso_ficha,
-          cantidad_ficha,
-          raza_ficha,
-          descripcion_ficha) 
-        VALUES (
-          :peso,
-          :cantidad,
-          :raza,
-          :descripcion)"
+    return $this->read(
+      "Ficha",
+      [
+        'id_ficha' => $id
+      ]
     );
-    $stm->execute([
-      'peso' => $fichaModel->getPeso(),
-      'cantidad' => $fichaModel->getCantidad(),
-      'raza' => $fichaModel->getRaza(),
-      'descripcion' => $fichaModel->getDescripcion(),
-    ]);
-    $lastInsertId = $this->_db->lastInsertId();
-    $this->_db = null;
-
-    return $lastInsertId;
   }
 
-  // ACTUALIZAR UNA FICHA
-  public function update($model)
+  public function addFicha($data)
   {
-    $stm = $this->_db->prepare(
-      "UPDATE usuarios
-        SET 
-          password_usuario = :password,
-          email_usuario = :email,
-          tipo_usuario = :tipo
-        WHERE username_usuario = :username"
-    );
-    $stm->execute([
-      'username' => $model->getUsername(),
-      'password' => $model->getPassword(),
-      'email' => $model->getEmail(),
-      'tipo' => $model->getTipo(),
-    ]);
-
-    $this->_db = null;
+    $this->create($data);
   }
-  // ELIMINAR UNA FICHA
-  public function delete($idFicha)
+
+  public function updateFicha($id, $data)
   {
-    $this->_db = DataBase::get();
-    $stm = $this->_db->prepare(
-      "DELETE FROM fichas WHERE id_ficha = :id"
-    );
-    $stm->execute([
-      'id' => $idFicha
-    ]);
-    $this->_db = null;
-    return $stm;
+    $this->update("id_ficha", $id, $data);
+  }
+
+  public function deleteFicha($id)
+  {
+    $this->delete("id_ficha", $id);
   }
 }
 ?>
