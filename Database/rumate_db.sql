9bf99375a497a6be8dc3fa7b1f98d9bd8aab3c24
Leandro <lewaccont@gmail.com>
2023-10-21 07:51:47 -0300

    eliminando y actualizando los scripts sql

--- Database/rumate_db.sql
+++ Database/rumate_db.sql
@@ -1,82 +1,80 @@
--- Crear la base de datos
-CREATE DATABASE rumate_db;
+-- Crear la base de datos rumate_db con cotejamiento utf8mb4_unicode_ci y motor InnoDB
+CREATE DATABASE rumate_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Usar la base de datos
+-- Usar la base de datos rumate_db
 USE rumate_db;
 
--- Tabla PERSONAS
+-- Crear la tabla PERSONAS
 CREATE TABLE PERSONAS (
-    id_persona INT AUTO_INCREMENT PRIMARY KEY,
-    nombre_persona VARCHAR(30) NOT NULL,
-    apellido_persona VARCHAR(30) NOT NULL,
-    ci_persona VARCHAR(15) NOT NULL,
-    barrio_persona VARCHAR(30) NOT NULL,
-    calle_persona VARCHAR(30) NOT NULL,
-    numero_persona VARCHAR(10) NOT NULL,
-    telefono_persona VARCHAR(15) NOT NULL,
-    tipo_persona VARCHAR(30) NOT NULL,
-    UNIQUE (ci_persona)
+    id_persona INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    nombre_persona VARCHAR(40) NOT NULL,
+    apellido_persona VARCHAR(40) NOT NULL,
+    ci_persona VARCHAR(40) UNIQUE NOT NULL,
+    barrio_persona VARCHAR(40),
+    calle_persona VARCHAR(40),
+    numero_persona VARCHAR(40),
+    telefono_persona VARCHAR(20) NOT NULL,
+    estado_persona VARCHAR(40)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Tabla USUARIOS
+-- Crear la tabla USUARIOS
 CREATE TABLE USUARIOS (
-    username_usuario VARCHAR(8) PRIMARY KEY,
+    id_usuario INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    username_usuario VARCHAR(40) UNIQUE NOT NULL,
     password_usuario VARCHAR(255) NOT NULL,
-    email_usuario VARCHAR(30) NOT NULL,
-    UNIQUE (email_usuario)
+    email_usuario VARCHAR(40) UNIQUE NOT NULL,
+    tipo_usuario ENUM('ROOT', 'ADMINISTRADOR', 'CLIENTE','PROVEEDOR','REMATADOR') DEFAULT 'CLIENTE'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Tabla USUARIOS_DE_PERSONAS
+-- Crear la tabla USUARIOS_DE_PERSONAS
 CREATE TABLE USUARIOS_DE_PERSONAS (
-    username_usuario_usuarios_de_personas VARCHAR(8) NOT NULL,
-    id_persona_usuarios_de_persona INT NOT NULL,
-    PRIMARY KEY (username_usuario_usuarios_de_personas, id_persona_usuarios_de_persona),
-    FOREIGN KEY (username_usuario_usuarios_de_personas) REFERENCES USUARIOS(username_usuario),
+    id_usuario_usuarios_de_personas INT NOT NULL,
+    id_persona_usuarios_de_persona INT UNIQUE NOT NULL,
+    PRIMARY KEY (id_usuario_usuarios_de_personas, id_persona_usuarios_de_persona),
+    FOREIGN KEY (id_usuario_usuarios_de_personas) REFERENCES USUARIOS(id_usuario),
     FOREIGN KEY (id_persona_usuarios_de_persona) REFERENCES PERSONAS(id_persona)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Tabla CLIENTES
-CREATE TABLE CLIENTES (
-    id_persona_cliente INT PRIMARY KEY,
-    estado_cliente VARCHAR (30) NOT NULL DEFAULT 'ACTIVO',
-    FOREIGN KEY (id_persona_cliente) REFERENCES PERSONAS(id_persona)
+-- Crear la tabla FICHAS
+CREATE TABLE FICHAS (
+    id_ficha INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    peso_ficha DECIMAL(10, 2) NOT NULL,
+    cantidad_ficha INT NOT NULL,
+    raza_ficha VARCHAR(40) NOT NULL,
+    descripcion_ficha TEXT
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Tabla REMATES
+-- Crear la tabla CATEGORIAS
+CREATE TABLE CATEGORIAS (
+    id_categoria INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    nombre_categoria VARCHAR(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Crear la tabla REMATES
 CREATE TABLE REMATES (
-    id_remate INT AUTO_INCREMENT PRIMARY KEY,
-    img_remate MEDIUMBLOB,
-    titulo_remate VARCHAR (30) NOT NULL,
-    fecha_remate DATE NOT NULL,
-    hora_remate TIME NOT NULL,
-    estado_remate VARCHAR(30) NOT NULL
+    id_remate INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    titulo_remate VARCHAR(40) NOT NULL,
+    imagen_remate VARCHAR(255),
+    fecha_inicio_remate DATETIME NOT NULL,
+    fecha_final_remate DATETIME NOT NULL,
+    estado_remate ENUM('Pendiente','Rematando','Finalizado') DEFAULT 'Pendiente'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Tabla LOTES
+-- Crear la tabla LOTES
 CREATE TABLE LOTES (
-    id_lote INT AUTO_INCREMENT PRIMARY KEY,
+    id_lote INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    imagen_lote VARCHAR(255),
     precio_base_lote DECIMAL(10, 2) NOT NULL,
-    precio_final_lote DECIMAL(10, 2) NOT NULL,
-    id_cliente_lote INT NOT NULL,
-    FOREIGN KEY (id_cliente_lote) REFERENCES CLIENTES(id_persona_cliente)
+    mejor_oferta_lote DECIMAL(10, 2) NOT NULL,
+    id_proveedor_lote INT NOT NULL,
+    id_ficha_lote INT UNIQUE NOT NULL,
+    id_categoria_lote INT NOT NULL,
+    FOREIGN KEY (id_proveedor_lote) REFERENCES PERSONAS(id_persona),
+    FOREIGN KEY (id_ficha_lote) REFERENCES FICHAS(id_ficha),
+    FOREIGN KEY (id_categoria_lote) REFERENCES CATEGORIAS(id_categoria)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Tabla MULTIMEDIAS
-CREATE TABLE MULTIMEDIAS (
-    id_multimedia INT AUTO_INCREMENT PRIMARY KEY,
-    contenido_multimedia MEDIUMBLOB
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- Tabla LOTE_MULTIMEDIA
-CREATE TABLE LOTE_MULTIMEDIA (
-    id_lote_lote_multimedia INT NOT NULL,
-    id_multimedia_lote_multimedia INT NOT NULL,
-    PRIMARY KEY (id_lote_lote_multimedia, id_multimedia_lote_multimedia),
-    FOREIGN KEY (id_lote_lote_multimedia) REFERENCES LOTES(id_lote),
-    FOREIGN KEY (id_multimedia_lote_multimedia) REFERENCES MULTIMEDIAS(id_multimedia)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- Tabla LOTES_POSTULAN_REMATES
+-- Crear la tabla LOTES_POSTULAN_REMATES
 CREATE TABLE LOTES_POSTULAN_REMATES (
     id_remate_lote_postula_remate INT NOT NULL,
     id_lote_lote_postula_remate INT NOT NULL,
@@ -85,62 +83,28 @@ CREATE TABLE LOTES_POSTULAN_REMATES (
     FOREIGN KEY (id_lote_lote_postula_remate) REFERENCES LOTES(id_lote)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Tabla PUJAS
+-- Crear la tabla PUJAS
 CREATE TABLE PUJAS (
-    id_puja INT AUTO_INCREMENT PRIMARY KEY,
+    id_puja INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     monto_puja DECIMAL(10, 2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Tabla PUJAS_DE_CLIENTES
-CREATE TABLE PUJAS_DE_CLIENTES (
-    id_puja_puja_de_cliente INT PRIMARY KEY,
-    id_cliente_puja_de_cliente INT NOT NULL,
-    FOREIGN KEY (id_puja_puja_de_cliente) REFERENCES PUJAS(id_puja),
-    FOREIGN KEY (id_cliente_puja_de_cliente) REFERENCES CLIENTES(id_persona_cliente)
+-- Crear la tabla PUJAS_DE_PERSONAS
+CREATE TABLE PUJAS_DE_PERSONAS (
+    id_puja_puja_de_persona INT NOT NULL,
+    id_persona_puja_de_persona INT NOT NULL,
+    PRIMARY KEY (id_puja_puja_de_persona, id_persona_puja_de_persona),
+    FOREIGN KEY (id_puja_puja_de_persona) REFERENCES PUJAS(id_puja),
+    FOREIGN KEY (id_persona_puja_de_persona) REFERENCES PERSONAS(id_persona)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Tabla PUJAS_DE_REMATES
+-- Crear la tabla PUJAS_DE_REMATES
 CREATE TABLE PUJAS_DE_REMATES (
-    id_puja_puja_de_remate INT PRIMARY KEY,
+    id_puja_puja_de_remate INT NOT NULL,
     id_remate_puja_de_remate INT NOT NULL,
     id_lote_puja_de_remate INT NOT NULL,
-    fecha_puja_de_remate DATE NOT NULL,
-    hora_puja_de_remate TIME NOT NULL,
-    UNIQUE (hora_puja_de_remate),
+    PRIMARY KEY (id_puja_puja_de_remate, id_remate_puja_de_remate, id_lote_puja_de_remate),
     FOREIGN KEY (id_puja_puja_de_remate) REFERENCES PUJAS(id_puja),
     FOREIGN KEY (id_remate_puja_de_remate) REFERENCES REMATES(id_remate),
     FOREIGN KEY (id_lote_puja_de_remate) REFERENCES LOTES(id_lote)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- Tabla CATEGORIAS
-CREATE TABLE CATEGORIAS (
-    id_categoria INT AUTO_INCREMENT PRIMARY KEY,
-    nombre_categoria VARCHAR(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- Tabla LOTES_CATEGORIZADOS
-CREATE TABLE LOTES_CATEGORIZADOS (
-    id_lote_lote_categorizado INT NOT NULL,
-    id_categoria_lote_categorizado INT NOT NULL,
-    PRIMARY KEY (id_lote_lote_categorizado, id_categoria_lote_categorizado),
-    FOREIGN KEY (id_lote_lote_categorizado) REFERENCES LOTES(id_lote),
-    FOREIGN KEY (id_categoria_lote_categorizado) REFERENCES CATEGORIAS(id_categoria)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- Tabla FICHAS
-CREATE TABLE FICHAS (
-    id_ficha INT AUTO_INCREMENT PRIMARY KEY,
-    peso_ficha DECIMAL(10, 2) NOT NULL,
-    cantidad_ficha INT NOT NULL,
-    raza_ficha VARCHAR(255) NOT NULL,
-    descripcion_ficha TEXT NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- Tabla LOTES_DESCRITOS_POR_FICHAS
-CREATE TABLE LOTES_DESCRITOS_POR_FICHAS (
-    id_ficha_lote_descrito_por_ficha INT NOT NULL,
-    id_lote_lote_descrito_por_ficha INT NOT NULL,
-    PRIMARY KEY (id_ficha_lote_descrito_por_ficha, id_lote_lote_descrito_por_ficha),
-    FOREIGN KEY (id_ficha_lote_descrito_por_ficha) REFERENCES FICHAS(id_ficha),
-    FOREIGN KEY (id_lote_lote_descrito_por_ficha) REFERENCES LOTES(id_lote)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
--- Database/rumate_db_test_data.sql
+++ Database/rumate_db_test_data.sql
@@ -1,110 +1,84 @@
--- Datos de prueba para la tabla PERSONAS
-INSERT INTO PERSONAS (nombre_persona, apellido_persona, ci_persona, barrio_persona, calle_persona, numero_persona, telefono_persona, tipo_persona)
+-- Insertar datos en la tabla PERSONAS
+INSERT INTO PERSONAS (nombre_persona, apellido_persona, ci_persona, barrio_persona, calle_persona, numero_persona, telefono_persona, estado_persona) 
 VALUES
-    ('Juan', 'Perez', '1234567', 'Centro', 'Calle 123', '123', '555-1234', 'ADMINISTRADOR'),
-    ('María', 'González', '7654321', 'Barrio Norte', 'Calle 456', '456', '555-5678', 'CLIENTE'),
-    ('Pedro', 'Ramírez', '9876543', 'Barrio Sur', 'Calle 789', '789', '555-7890', 'PROVEEDOR');
+    ('Juan', 'Perez', '12345678', 'Barrio A', 'Calle 1', '123', '123-456-789', 'Activo'),
+    ('María', 'López', '87654321', 'Barrio B', 'Calle 2', '456', '987-654-321', 'Inactivo'),
+    ('Pedro', 'García', '55555555', 'Barrio C', 'Calle 3', '789', '555-555-555', 'Activo'),
+    ('Laura', 'Gómez', '98765432', 'Barrio D', 'Calle 4', '567', '222-333-444', 'Activo'),
+    ('Carlos', 'López', '12344321', 'Barrio E', 'Calle 5', '678', '999-888-777', 'Inactivo');
 
--- Datos de prueba para la tabla USUARIOS
-INSERT INTO USUARIOS (username_usuario, password_usuario, email_usuario)
+-- Insertar datos en la tabla USUARIOS
+INSERT INTO USUARIOS (username_usuario, password_usuario, email_usuario, tipo_usuario) 
 VALUES
-    ('juan123', 'password1', 'juan@example.com'),
-    ('maria456', 'password2', 'maria@example.com'),
-    ('pedro789', 'password3', 'pedro@example.com');
+    ('root', '$2y$10$C0bPJ9Itstkm4eDlsd52QuVDkR4oH3gPVgdp0usmS81qho8YSxOTi', 'root@example.com', 'ROOT'),
+    ('admin', '$2y$10$hbjEYjPcKVQ2.IIX4DWk5eJGEOIDAZx.vaf39ILQ1iEFwmBtMSCPi', 'admin@example.com', 'ADMINISTRADOR'),
+    ('cliente', '$2y$10$xoTV71k203SRSjR/B1zkPurXezKwWExF8Vt4rXKF7vyzo1YjVj1d6', 'cliente@example.com', 'CLIENTE'),
+    ('proveedor', '$2y$10$Bpjp/rLggmhvv1wwHv.7C.kyb2dT/Gf3qMR3.oUQskEjGP3xrxrhi', 'proveedor@example.com', 'PROVEEDOR'),
+    ('rematador', '$2y$10$E/loDqHzCjJ1X13oR6FIuOxELntpmwGWmUrzsVbOYYnwG1t8ULxhG', 'rematador@example.com', 'REMATADOR');
 
--- Asociar usuarios a personas
-INSERT INTO USUARIOS_DE_PERSONAS (username_usuario_usuarios_de_personas, id_persona_usuarios_de_persona)
-VALUES
-    ('juan123', 1),
-    ('maria456', 2),
-    ('pedro789', 3);
-
--- Datos de prueba para la tabla CLIENTES
-INSERT INTO CLIENTES (id_persona_cliente, estado_cliente)
-VALUES
-    (2, 'ACTIVO'),
-    (3, 'ACTIVO');
-
--- Datos de prueba para la tabla REMATES
-INSERT INTO REMATES (img_remate, titulo_remate, fecha_remate, hora_remate, estado_remate)
-VALUES
-    (NULL, 'Remate de Vacas 1', '2023-09-20', '14:00:00', 'FINALIZADO'),
-    (NULL, 'Remate de Vacas 2', '2023-09-25', '15:30:00', 'PENDIENTE'),
-    (NULL, 'Remate de Vacas 3', '2023-10-05', '16:15:00', 'PENDIENTE');
-
--- Datos de prueba para la tabla LOTES
-INSERT INTO LOTES (precio_base_lote, precio_final_lote, id_cliente_lote)
-VALUES
-    (1500.00, 1700.00, 3),
-    (1800.00, 2000.00, 3),
-    (1600.00, 1800.00, 3);
-
--- Datos de prueba para la tabla MULTIMEDIAS
-INSERT INTO MULTIMEDIAS (contenido_multimedia)
-VALUES
-    (NULL),
-    (NULL),
-    (NULL);
-
--- Asociar lotes con multimedia
-INSERT INTO LOTE_MULTIMEDIA (id_lote_lote_multimedia, id_multimedia_lote_multimedia)
+-- Insertar datos en la tabla USUARIOS_DE_PERSONAS
+INSERT INTO USUARIOS_DE_PERSONAS (id_usuario_usuarios_de_personas, id_persona_usuarios_de_persona)
 VALUES
     (1, 1),
     (2, 2),
-    (3, 3);
+    (3, 3),
+    (4, 4),
+    (5, 5);
 
--- Datos de prueba para la tabla LOTES_POSTULAN_REMATES
+-- Insertar datos en la tabla FICHAS
+INSERT INTO FICHAS (peso_ficha, cantidad_ficha, raza_ficha, descripcion_ficha)
+VALUES
+    (250.00, 10, 'Angus', 'Lotes de terneros Angus de excelente calidad.'),
+    (300.00, 5, 'Hereford', 'Vaquillonas Hereford, 2 años de edad.'),
+    (500.00, 2, 'Holstein', 'Vacunas Holstein en buen estado.');
+
+-- Insertar datos en la tabla CATEGORIAS
+INSERT INTO CATEGORIAS (nombre_categoria)
+VALUES
+    ('Ternero'),
+    ('Vaquillona'),
+    ('Vaca'),
+    ('Novillito'),
+    ('Novillo'),
+    ('Toro');
+
+-- Insertar datos en la tabla REMATES
+INSERT INTO REMATES (titulo_remate, fecha_inicio_remate, fecha_final_remate)
+VALUES
+    ('Remate 1', '2023-10-20 10:00:00', '2023-10-21 14:00:00'),
+    ('Remate 2', '2023-10-22 09:00:00', '2023-10-23 15:00:00');
+
+-- Insertar datos en la tabla LOTES
+INSERT INTO LOTES (imagen_lote, precio_base_lote, mejor_oferta_lote, id_proveedor_lote, id_ficha_lote, id_categoria_lote)
+VALUES
+    ('lote1.jpg', 1000.00, 1200.00, 4, 1, 1),
+    ('lote2.jpg', 800.00, 900.00, 4, 2, 2),
+    ('lote3.jpg', 1500.00, 1600.00, 4, 3, 3);
+
+-- Insertar datos en la tabla LOTES_POSTULAN_REMATES
 INSERT INTO LOTES_POSTULAN_REMATES (id_remate_lote_postula_remate, id_lote_lote_postula_remate)
 VALUES
     (1, 1),
-    (2, 2),
-    (3, 3);
+    (1, 2),
+    (2, 3);
 
--- Datos de prueba para la tabla PUJAS
+-- Insertar datos en la tabla PUJAS
 INSERT INTO PUJAS (monto_puja)
 VALUES
-    (1750.00),
-    (1950.00),
-    (1850.00);
+    (1100.00),
+    (1000.00),
+    (1600.00);
 
--- Datos de prueba para la tabla PUJAS_DE_CLIENTES
-INSERT INTO PUJAS_DE_CLIENTES (id_puja_puja_de_cliente, id_cliente_puja_de_cliente)
+-- Insertar datos en la tabla PUJAS_DE_PERSONAS
+INSERT INTO PUJAS_DE_PERSONAS (id_puja_puja_de_persona, id_persona_puja_de_persona)
 VALUES
-    (1, 2),
+    (1, 3),
     (2, 3),
-    (3, 2);
-
--- Datos de prueba para la tabla PUJAS_DE_REMATES
-INSERT INTO PUJAS_DE_REMATES (id_puja_puja_de_remate, id_remate_puja_de_remate, id_lote_puja_de_remate, fecha_puja_de_remate, hora_puja_de_remate)
-VALUES
-    (1, 1, 1, '2023-09-20', '14:30:00'),
-    (2, 2, 2, '2023-09-25', '15:45:00'),
-    (3, 3, 3, '2023-10-05', '16:30:00');
-
--- Datos de prueba para la tabla CATEGORIAS
-INSERT INTO CATEGORIAS (nombre_categoria)
-VALUES
-    ('Terneros'),
-    ('Vaquillonas'),
-    ('Novillos');
-
--- Asociar lotes con categorías
-INSERT INTO LOTES_CATEGORIZADOS (id_lote_lote_categorizado, id_categoria_lote_categorizado)
-VALUES
-    (1, 1),
-    (2, 2),
     (3, 3);
 
--- Datos de prueba para la tabla FICHAS
-INSERT INTO FICHAS (peso_ficha, cantidad_ficha, raza_ficha, descripcion_ficha)
+-- Insertar datos en la tabla PUJAS_DE_REMATES
+INSERT INTO PUJAS_DE_REMATES (id_puja_puja_de_remate, id_remate_puja_de_remate, id_lote_puja_de_remate)
 VALUES
-    (500.00, 10, 'Holstein', 'Lote de vacas Holstein de alta producción de leche.'),
-    (450.00, 8, 'Angus', 'Lote de vacas Angus criadas para carne de alta calidad.'),
-    (550.00, 12, 'Jersey', 'Lote de vacas Jersey ideales para la producción de queso.');
-
--- Asociar fichas con lotes
-INSERT INTO LOTES_DESCRITOS_POR_FICHAS (id_ficha_lote_descrito_por_ficha, id_lote_lote_descrito_por_ficha)
-VALUES
-    (1, 1),
-    (2, 2),
-    (3, 3);
+    (1, 1, 1),
+    (2, 1, 2),
+    (3, 2, 3);
