836500b13807521494597376dfd03039fd0713a2
Leandro <lewaccont@gmail.com>
2023-10-27 11:21:51 -0300

    ahora se ve el username del proveedor en el titulo del lote

--- Resources/Views/Lote/card-lote.php
+++ Resources/Views/Lote/card-lote.php
@@ -3,9 +3,11 @@
     alt="Imagen de remate">
   <div class="card-remate__info">
     <h2 class="card-remate__title">LOTE de
-      <?= $lote->getCategoria()->getNombre(); ?>s</h2>
+      <?= $lote->getCategoria()->getNombre(); ?>s
+      <a href=""><span>#<?= $lote->getProveedor()->getUsername(); ?></span></a>
+    </h2>
     <p class="card-remate__data"><b>Proveedor:
-      </b><?= $lote->getProveedor()->getUsername(); ?>
+      </b><?= $lote->getCategoria()->getNombre(); ?>
     </p>
     <p class="card-remate__data"><b>Cantidad:
       </b><?= $lote->getFicha()->getCantidad(); ?></p>
