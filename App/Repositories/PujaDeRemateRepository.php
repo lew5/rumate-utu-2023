ac32e360467ceb52bad42427cdd131978d900c70
Leandro <lewaccont@gmail.com>
2023-11-06 17:38:46 -0300

    repo done

--- App/Repositories/PujaDeRemateRepository.php
+++ App/Repositories/PujaDeRemateRepository.php
@@ -1,17 +1,35 @@
 <?php
-
+/**
+ * Clase PujaDeRemateRepository
+ *
+ * Esta clase extiende la clase Repository y proporciona métodos específicos para interactuar con la tabla "pujas_de_remates" en la base de datos.
+ */
 class PujaDeRemateRepository extends Repository
 {
+  /**
+   * Constructor de la clase PujaDeRemateRepository.
+   */
   public function __construct()
   {
-    parent::__construct("pujas_de_remates","id_puja_puja_de_remate","PujaDeRemate");
+    parent::__construct("pujas_de_remates", "id_puja_puja_de_remate", "PujaDeRemate");
   }
 
+  /**
+   * Recupera todos los registros de pujas de remates de la base de datos.
+   *
+   * @return mixed|null Devuelve una lista de objetos que representan todos los registros de pujas de remates o null si no se encontraron registros.
+   */
   public function find()
   {
     return $this->read();
   }
 
+  /**
+   * Busca un registro de puja de remate por su ID en la base de datos.
+   *
+   * @param mixed $id El ID de la puja de remate que se desea buscar.
+   * @return mixed|null Devuelve un objeto que representa el registro de la puja de remate encontrado por su ID o null si no se encontró ningún registro.
+   */
   public function findByPujaId($id)
   {
     return $this->read(
@@ -21,19 +39,34 @@ class PujaDeRemateRepository extends Repository
     );
   }
 
+  /**
+   * Agrega un nuevo registro de puja de remate a la tabla "pujas_de_remates" en la base de datos.
+   *
+   * @param array $data Un arreglo asociativo que contiene los datos del nuevo registro de puja de remate.
+   */
   public function addPujaDeRemate($data)
   {
     $this->create($data);
   }
 
+  /**
+   * Actualiza un registro existente en la tabla "pujas_de_remates" en la base de datos.
+   *
+   * @param mixed $id El ID del registro de puja de remate que se va a actualizar.
+   * @param array $data Un arreglo asociativo que contiene los datos actualizados del registro de puja de remate.
+   */
   public function updatePujaDeRemate($id, $data)
   {
     $this->update($id, $data);
   }
 
+  /**
+   * Elimina un registro de puja de remate de la tabla "pujas_de_remates" en la base de datos.
+   *
+   * @param mixed $id El ID del registro de puja de remate que se va a eliminar.
+   */
   public function deletePujaDeRemate($id)
   {
     $this->delete($id);
   }
 }
-?>
--- App/Repositories/PujaRepository.php
+++ App/Repositories/PujaRepository.php
@@ -1,17 +1,35 @@
 <?php
-
+/**
+ * Clase PujaRepository
+ *
+ * Esta clase extiende la clase Repository y proporciona métodos específicos para interactuar con la tabla "pujas" en la base de datos.
+ */
 class PujaRepository extends Repository
 {
+  /**
+   * Constructor de la clase PujaRepository.
+   */
   public function __construct()
   {
-    parent::__construct("pujas","id_puja","Puja");
+    parent::__construct("pujas", "id_puja", "Puja");
   }
 
+  /**
+   * Recupera todos los registros de pujas de la base de datos.
+   *
+   * @return mixed|null Devuelve una lista de objetos que representan todos los registros de pujas o null si no se encontraron registros.
+   */
   public function find()
   {
     return $this->read();
   }
 
+  /**
+   * Busca un registro de puja por su ID en la base de datos.
+   *
+   * @param mixed $id El ID de la puja que se desea buscar.
+   * @return mixed|null Devuelve un objeto que representa el registro de la puja encontrado por su ID o null si no se encontró ningún registro.
+   */
   public function findById($id)
   {
     return $this->read(
@@ -21,19 +39,34 @@ class PujaRepository extends Repository
     );
   }
 
+  /**
+   * Agrega un nuevo registro de puja a la tabla "pujas" en la base de datos.
+   *
+   * @param array $data Un arreglo asociativo que contiene los datos del nuevo registro de puja.
+   */
   public function addPuja($data)
   {
     $this->create($data);
   }
 
+  /**
+   * Actualiza un registro existente en la tabla "pujas" en la base de datos.
+   *
+   * @param mixed $id El ID del registro de puja que se va a actualizar.
+   * @param array $data Un arreglo asociativo que contiene los datos actualizados del registro de puja.
+   */
   public function updatePuja($id, $data)
   {
     $this->update($id, $data);
   }
 
+  /**
+   * Elimina un registro de puja de la tabla "pujas" en la base de datos.
+   *
+   * @param mixed $id El ID del registro de puja que se va a eliminar.
+   */
   public function deletePuja($id)
   {
     $this->delete($id);
   }
 }
-?>
